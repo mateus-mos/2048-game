46aff9953e22589472651452ca8c39c3ccfe7dd1
Mateus <mateus.silvaoliveira13@outlook.com>
2021-03-02 12:23:11 -0300

    Add movement and sum if they are equal.

--- game2084.pas
+++ game2084.pas
@@ -1,8 +1,12 @@
 program game2084;
 const
     MAX_MAP = 8;
-    WALL = 5;
+    WALL = -5;
     NOTHING = 0;
+    UP = 93;
+    RIGHT = 92;
+    LEFT = 91;
+    DOWN = 90;
 type 
     type_game = record 
 	lin,col:integer;
@@ -13,7 +17,10 @@ type
 	lin,col:integer;
     end;
 
-var game:type_game;
+var 
+    game:type_game;
+    action:integer;
+    end_game:boolean;
 
 procedure Create_Two(var game:type_game);
 (* Create the number two in a random position on the map *)
@@ -62,7 +69,107 @@ begin
 	end;
 end;
 
+function IsSpaceFree(var game:type_game; direction,i,j:integer):boolean;
+(* Returns true If the next direction is free *)
+(* Otherwise returns false *)
 begin
+    case direction of
+	UP:IsSpaceFree:= game.map[i-1,j] = NOTHING;
+	RIGHT:IsSpaceFree:= game.map[i,j+1] = NOTHING;
+	LEFT:IsSpaceFree:= game.map[i,j-1] = NOTHING;
+	DOWN:IsSpaceFree:= game.map[i+1,j] = NOTHING;
+    end;
+end;
+
+function MoveNumber(var game:type_game; i,j,k,l,direction:integer):boolean;
+(* Move the number in position [i,j] to position [k,l] *)
+(* returns true If the movement is possible*)
+(* returns false If the movement is not possible *)
+(* sum them If the position [k,l] have an igual number *)
+begin
+    MoveNumber:=false;
+    if game.map[i,j] > 0 then
+	begin
+	    if IsSpaceFree(game,direction,i,j) then
+		begin
+		    MoveNumber:=true;
+		    game.map[k,l]:=game.map[i,j];
+		    game.map[i,j]:=NOTHING;
+		end
+	    else if game.map[k,l] = game.map[i,j] then
+		begin
+		    MoveNumber:=true;
+		    game.map[k,l]:=game.map[i,j]*2;
+		    game.map[i,j]:=NOTHING;
+		end;
+	end;
+end;
+
+function move_up(var game:type_game):boolean;
+var i,j:integer;
+begin
+    move_up:=false;
+    with game do
+	begin
+	    for i:=lin downto 2 do
+		for j:=col downto 1 do
+		    MoveNumber(game,i,j,i-1,j,UP);
+	end;
+end;
+
+function move_down(var game:type_game):boolean;
+var i,j:integer;
+begin
+    move_down:=false;
+    with game do
+	begin
+	    for i:=2 to lin do
+		for j:=1 to col do
+		    MoveNumber(game,i,j,i+1,j,DOWN);
+	end;
+end;
+
+function move_right(var game:type_game):boolean;
+var i,j:integer;
+begin
+    move_right:=false;
+    with game do
+	begin
+	    for i:=2 to lin do
+		for j:=1 to col do
+		    MoveNumber(game,i,j,i,j+1,RIGHT);
+	end;
+end;
+
+function move_left(var game:type_game):boolean;
+var i,j:integer;
+begin
+    move_left:=false;
+    with game do
+	begin
+	    for i:=lin downto 2 do
+		for j:=col downto 1 do
+		    MoveNumber(game,i,j,i,j-1,LEFT);
+	end;
+end;
+
+
+
+begin
+    end_game:=false;
     initializing_game(game);
-    print_map(game);
+
+    repeat 
+	print_map(game);
+	readln(action);
+	case action of
+	    UP:move_up(game);
+	    RIGHT:move_right(game);
+	    LEFT:move_left(game);
+	    DOWN:move_down(game);
+	    else
+		end_game:=true;
+	end;
+
+    until end_game;
 end.
